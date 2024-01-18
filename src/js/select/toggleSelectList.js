b4d49d2d0815b7e62071f9b978fcecc45a54ba8c
haranycheva <mimimashkabumazka@gmail.com>
2024-01-18 16:02:55 +0200

    fix

--- src/js/select/toggleSelectList.js
+++ src/js/select/toggleSelectList.js
@@ -1,7 +1,4 @@
 import { refs } from "./refs.js";
 export function toggleSelectList(){
     refs.selectList.classList.toggle("hide-select");
-    // if(!refs.selectList.classList.contains("is-hidden")){
-    //     refs.selectList.style.display = "block"
-    // }
 }
